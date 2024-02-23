893cb5b31c0a12b97dd4b0530444891df158d148
Hélène BAUDRAND <Helene@MacBook-Pro-de-apixis.local>
2024-02-23 12:01:59 +0100

    feat(installed libs): typeorm nestjs config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './core/auth/auth.module';
 import { UserModule } from './user/user.module';
-
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.POSTGRES_HOST,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule,
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
